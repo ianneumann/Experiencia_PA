53c1a5e90d57dc53f0bea4419177092156fe5fe7
ianneumann <163368644+ianneumann@users.noreply.github.com>
2024-03-14 19:30:43 +0000

    termine el juego memoria

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,3 +1,5 @@
+import random
+
 def memoria():
     """
     Esta función representa el juego de memoria.
@@ -5,4 +7,26 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    dif = str(input('Elegir dificultad: facil/medio/dificil '))
+    if dif == 'facil':
+        dif = 3
+    elif dif == 'medio':
+        dif = 5
+    else:
+        dif = 10
+    print('Iniciando la secuencia...')
+    sec_c = []
+    for i in range(dif):
+        num = random.randint(1,10)
+        sec_c.append(num)
+    print(sec_c)
+
+    sec_u = []
+    for i in range(dif):
+        num = int(input('Insertar numero: '))
+        sec_u.append(num)
+
+    if sec_c == sec_u:
+        print('Enhorabuena! Eres el repetidor de secuencias johnson')
+    else:
+        print('Hace falta entrenar la memoria broski')
